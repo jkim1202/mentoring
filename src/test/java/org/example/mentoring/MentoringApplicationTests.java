e704de38278d25ae945571016696830b2f1c3504
jkim1202 <junhyuk317@gmail.com>
2026-03-16 18:17:41 +0900

    refactor(reservation): active_slot_id virtual column 적용

--- src/test/java/org/example/mentoring/MentoringApplicationTests.java
+++ src/test/java/org/example/mentoring/MentoringApplicationTests.java
@@ -1,13 +1,11 @@
 package org.example.mentoring;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-@Disabled
 class MentoringApplicationTests {
 
     @Test
